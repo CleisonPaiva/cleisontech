3c2743ac7ef85b17c6838fa02eeb9661c3bfc955
Cleison Mendes <cleison606@gmail.com>
2025-05-30 16:44:19 -0300

    Update back button links to use relative paths and add new Sao Paulo Web Scene files

--- samples/03-saoPauloWebScene/saoPauloWebScene.js
+++ samples/03-saoPauloWebScene/saoPauloWebScene.js
@@ -1,24 +1,23 @@
-require(["esri/config", "esri/views/SceneView"], function(esriConfig, SceneView) {
+let view
+require(["esri/config", "esri/views/SceneView"], function (esriConfig, SceneView) {
             esriConfig.apiKey = "AAPTxy8BH1VEsoebNVZXo8HurMrIUxu4Ip2HSeoi2rKHLyOAdY3ja5z63C-Dw1Dx2-MBdu1BHSJghGdj7MepUlsgu8Na2IlvzGkQuh5qEmbRQDGtDabEo1a-fCzyXwse7A7bLO_HOE7PhXVT5SXBqfkqK1Yr6KSqDmyyOrcoD_Hl4_2bmasqdHkYWHcyTif4pvhGhQP-Vup_DX50phd8w8TLe7LtQy7FF3FgOxpG-GrkSic.AT1_83Q1q6hR";
 
             const arcgisScene = document.querySelector("arcgis-scene");
 
             arcgisScene.addEventListener("arcgisViewReadyChange", () => {
-                const window.view = arcgisScene.view;
+                view = arcgisScene.view;
 
                 view.camera = {
-                    position: [
-                        -40.313,
-                       -20.288,
-                        300],
+                    position: [-46.65013896800718,-23.52903991025441, 2000],
                     heading: 77.40,
                     tilt: 77.25
-                  };
+                };
 
                 view.environment.lighting = {
                     type: "sun",
                     date: new Date("January 1, 2022 15:00:00 UTC"),
+                    directShadowsEnabled: true
                 };
-                view.environment.lighting.directShadowsEnabled = true;
-            });
+                });
+
          });
